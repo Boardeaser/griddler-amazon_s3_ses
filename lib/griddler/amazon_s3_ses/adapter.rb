5e52d738817a0aaff5a4800fa713e74385a36fb9
Jakob Waller <jakob@appily.se>
2020-08-21 17:26:49 +0200

    Added config options for aws credentials

--- lib/griddler/amazon_s3_ses/adapter.rb
+++ lib/griddler/amazon_s3_ses/adapter.rb
@@ -143,7 +143,11 @@ module Griddler
       end
 
       def s3
-        @s3 ||= Aws::S3::Resource.new(region: Griddler::AmazonS3SES.configuration.aws_region)
+        @s3 ||= Aws::S3::Resource.new({
+                  region: Griddler::AmazonS3SES.configuration.aws_region,
+                  access_key_id: Griddler::AmazonS3SES.configuration.access_key_id,
+                  secret_access_key: Griddler::AmazonS3SES.configuration.secret_access_key,
+                })
       end
 
       def email_s3_object
